d387b31248e7c5ddfb41537613c3f2883bc28f2b
Kenneth Peeples <kpeeples@redhat.com>
2013-10-16 20:47:37 -0400

    initial checkin

---
+++ lab2/src/main/java/mortgages/QualificationServiceBean.java
@@ -0,0 +1,36 @@
+package mortgages;
+
+import javax.inject.Inject;
+
+import org.switchyard.component.bean.Property;
+import org.switchyard.component.bean.Reference;
+import org.switchyard.component.bean.Service;
+
+@Service(QualificationService.class)
+public class QualificationServiceBean implements QualificationService {
+    
+    private static final String EASY = "easy";
+    
+    @Property(name = "creditTerms")
+    private String creditTerms;
+    
+    @Inject @Reference
+    private Audit audit;
+    
+    @Override
+    public Applicant qualify(Applicant applicant) {
+    	
+    	System.out.println("Qualifying applicant: " + applicant);
+    	
+        if (EASY.equals(creditTerms)) {
+            applicant.setApproved(true);
+        } else {
+            applicant.setApproved(applicant.getCreditScore() >= 600);
+        }
+        
+        audit.recordApplication(applicant);
+    	
+        return applicant;
+    }
+
+}
